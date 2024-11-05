85f8afad238b04d7baf8f414df325a1a562dac19
Brian J Brennan <brianloveswords@gmail.com>
2024-11-04 21:48:40 -0500

    better secret management in cloudflare cache purge

--- scripts/cloudflare-cache-purge.sh
+++ scripts/cloudflare-cache-purge.sh
@@ -3,12 +3,32 @@
 set -euo pipefail
 
 function main() {
-    CLOUDFLARE_API_TOKEN=$(op read "op://private/Cloudflare/api token")
-    CLOUDFLARE_ZONE=$(op read "op://private/Cloudflare/zone")
+    # this syntax for setting a default value if the variable is not set
+    # is absolutely ridiculous.
+    : ${READY_TO_PURGE_CACHE:="false"}
 
-    curl -X POST "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE}/purge_cache" \
+    if [[ "${READY_TO_PURGE_CACHE}" == "true" ]]; then
+        purge_cache
+    else
+        setup
+    fi
+}
+
+function setup() {
+    echo "cloudflare-purge-cache: setting up environment variables"
+    export CLOUDFLARE_API_TOKEN="op://private/cloudflare/api token"
+    export CLOUDFLARE_ZONE="op://private/cloudflare/zone"
+    export READY_TO_PURGE_CACHE=true
+
+    op run -- ${0}
+}
+
+function purge_cache() {
+    echo "cloudflare-purge-cache: firing the lasers"
+    curl -X POST --silent \
+        "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE}/purge_cache" \
         -H "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" \
-    	-H "Content-Type: application/json" \
+        -H "Content-Type: application/json" \
         --data '{"purge_everything":true}'
 }
 
