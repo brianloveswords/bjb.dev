7a5e1ec440b2dc0c1a444bdc785cf28fe566aedc
Brian J Brennan <brianloveswords@gmail.com>
2025-01-05 23:32:37 -0500

    [blog] allow sticking an image in header (#13)
    also fix some paths with the wrong date!

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,7 +9,7 @@ const log = defineCollection({
     // Transform string to Date object
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
+    image: z.string().optional(),
     unlisted: z.boolean().optional(),
   }),
 });
